951242498255c83a4b85310532aa1c531c03e170
Asaadziad <asaadabuelhija@gmail.com>
2023-11-18 08:23:18 +0200

    collision detection - improvement

--- src/CollisionDetection.c
+++ src/CollisionDetection.c
@@ -10,6 +10,15 @@ bool checkCollision(Player p, SDL_Rect dst){
     if(src.x + src.w >= dst.x && (src.x < dst.x) && (src.y >= dst.y && src.y <= dst.y + dst.h)){
         p->position.x = dst.x - p->width;
     }
+    if(src.x <= dst.x + dst.w && src.x + src.w > dst.x + dst.w && (src.y >= dst.y && src.y <= dst.y + dst.h)){
+        p->position.x = dst.x + dst.w;
+    }
+    if(src.y <= dst.y + dst.h && src.y + src.h > dst.y + dst.h && (src.x >= dst.x && src.x <= dst.x + dst.w)){
+        p->position.y = dst.y + dst.h - 10;
+    }
+    if(src.y + src.h >= dst.y && src.y < dst.y && (src.x >= dst.x && src.x <= dst.x + dst.w)){
+        p->position.y = dst.y - p->height;
+    }
     if((src.x + src.w > dst.x) 
     && (src.x < dst.x + dst.w)
     && (src.y + src.h > dst.y) 
