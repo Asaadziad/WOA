f9fcde80e74ef61cc0dc10f70d2c1833c06a72af
Asaadziad <asaadabuelhija@gmail.com>
2023-12-09 12:54:38 +0200

    mental

--- linux/linux_woa.cpp
+++ linux/linux_woa.cpp
@@ -1,6 +1,8 @@
 #include <X11/X.h>
+#include <X11/XKBlib.h>
 #include <iostream>
 #include <X11/Xlib.h>
+#include <X11/keysymdef.h>
 
 int main(){
 
@@ -20,10 +22,13 @@ int main(){
 		if(XPending(display)){
 			XNextEvent(display, &event);
 			if(event.type == KeyPress){
+			//TODO:: find a way to deal with key presses without hardcoding it gkdsljjkldgjslk			
 			switch (event.xkey.keycode) {
-				case 9:
+				case 0x09:
 				running = false;
 				break;
+			
+			default:break;
 			}	
 			}
 		}
