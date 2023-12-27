650d2a093bf75d35c7bbf6974ed55c046516b3bf
Asaadziad <asaadabuelhija@gmail.com>
2023-12-27 22:57:17 +0200

    basic htable

--- woa/src/DS/hashtable.c
+++ woa/src/DS/hashtable.c
@@ -1,4 +1,37 @@
 #include "hashtable.h"
+#include "stddef.h"
+
+#define START_SIZE 8
+
+typedef struct {
+  KEY key;
+  VAL value;
+} Entry;
+
+// TODO:: let the user define a destroy function
+static void destroyEntry(Entry e){}
+
+struct hash_t {
+  size_t size;
+  Entry* entries; 
+};
 
 
+// Creates an empty table of size - START_SIZE
+HashTable createTable(){
+  HashTable t = (HashTable)malloc(sizeof(*t));
+  if(!t) return NULL;
+  t->size = 0;
+  t->entries = (Entry*)malloc(sizeof(Entry) * START_SIZE);
+  if(!t->entries) return NULL;
+  return t;
+}
 
+void destroyTable(HashTable table){
+  if(!table) return;
+  for(int i = 0;i < table->size;i++){
+    destroyEntry(table->entries[i]);
+  }
+  free(table->entries);
+  free(table);
+}
