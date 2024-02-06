5ce11ba9fc74698359760b46c7972a1f80bb55c5
Asaadziad <asaadabuelhija@gmail.com>
2024-02-06 22:06:13 +0200

    .

---
+++ woa/engine/src/ProjectileManager.c
@@ -0,0 +1,20 @@
+#include "../../headers/ProjectileManager.h"
+#include "stdlib.h"
+#include "../../headers/hashtable.h"
+
+struct porjectile_manager_t {
+  HashTable projectile_table;  
+};
+
+
+ProjectileManager initProjectileManager() {
+  ProjectileManager manager = (ProjectileManager)malloc(sizeof(ProjectileManager));
+  if(!manager) return NULL;
+  manager->projectile_table = createTable();
+  return manager;
+}
+
+void destroyProjectileManager(ProjectileManager p_manager){}
+
+void projectileUpdate(ProjectileManager p_manager){}
+
